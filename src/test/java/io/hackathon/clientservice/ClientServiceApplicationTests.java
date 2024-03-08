01e97859a1c8db9ec81453fdc87176818fce6c1e
Grzegorz Oladele <grzegorzoladele@MacBook-Pro-Grzegorz.local>
2024-03-08 20:21:52 +0100

    Init project

---
+++ src/test/java/io/hackathon/clientservice/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hackathon.clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
